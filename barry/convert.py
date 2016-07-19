060167851ec0e117c1e1c476e8e1115e4d102351
jyotiska <jyotiska123@gmail.com>
2016-07-19 15:20:31 +0530

    added docstrings to convert methods

--- barry/convert.py
+++ barry/convert.py
@@ -1,7 +1,18 @@
-from exceptions import BarryFileException
+from exceptions import BarryFileException, BarryConversionException, BarryExportException
 import pandas as pd
 
+
 def detect_file_extension(filename):
+    """Extract and return the extension of a file given a filename.
+
+    Args:
+        filename (str): name of the file
+    Returns:
+        str: extension of the file
+    Raises:
+        BarryFileException: if extension not present in filename
+    """
+
     if filename is None:
         raise BarryFileException("Input file name cannot be None")
 
@@ -14,28 +25,115 @@ def detect_file_extension(filename):
 
 
 def xls_to_df(filename):
-    return pd.read_excel(filename)
+    """Converts a XLS file to Pandas dataframe.
+
+    Args:
+        filename (str): name of the file
+    Returns:
+        dataframe: a pandas dataframe
+    Raises:
+        BarryConversionException: if file cannot be converted to dataframe
+    """
+
+    try:
+        return pd.read_excel(filename)
+    except Exception as e:
+        raise BarryConversionException("Could not convert file %s to dataframe" % (filename))
 
 
 def xlsx_to_df(filename):
-    return pd.read_excel(filename)
+    """Converts a XLSX file to Pandas dataframe.
+
+    Args:
+        filename (str): name of the file
+    Returns:
+        dataframe: a pandas dataframe
+    Raises:
+        BarryConversionException: if file cannot be converted to dataframe
+    """
+
+    try:
+        return pd.read_excel(filename)
+    except Exception as e:
+        raise BarryConversionException("Could not convert file %s to dataframe" % (filename))
 
 
 def csv_to_df(filename):
-    return pd.read_csv(filename)
+    """Converts a CSV file to Pandas dataframe.
+
+    Args:
+        filename (str): name of the file
+    Returns:
+        dataframe: a pandas dataframe
+    Raises:
+        BarryConversionException: if file cannot be converted to dataframe
+    """
+
+    try:
+        return pd.read_csv(filename)
+    except Exception as e:
+        raise BarryConversionException("Could not convert file %s to dataframe" % (filename))
 
 
 def df_to_xls(df, out_filename):
-    df.to_excel(out_filename)
+    """Writes a Pandas dataframe to a XLS file.
+
+    Args:
+        df (dataframe): dataframe to be written to file
+        filename (str): name of the file
+    Raises:
+        BarryExportException: if file cannot be converted to dataframe
+    """
+
+    try:
+        df.to_excel(out_filename)
+    except Exception as e:
+        raise BarryExportException("Could not write dataframe to file %s" % (out_filename))
 
 
 def df_to_xlsx(df, out_filename):
-    df.to_excel(out_filename)
+    """Writes a Pandas dataframe to a XLS file.
+
+    Args:
+        df (dataframe): dataframe to be written to file
+        filename (str): name of the file
+    Raises:
+        BarryExportException: if file cannot be converted to dataframe
+    """
+
+    try:
+        df.to_excel(out_filename)
+    except Exception as e:
+        raise BarryExportException("Could not write dataframe to file %s" % (out_filename))
 
 
 def df_to_json(df, out_filename):
-    df.to_json(out_filename)
+    """Writes a Pandas dataframe to a JSON file.
+
+    Args:
+        df (dataframe): dataframe to be written to file
+        filename (str): name of the file
+    Raises:
+        BarryExportException: if file cannot be converted to dataframe
+    """
+
+    try:
+        df.to_json(out_filename)
+    except Exception as e:
+        raise BarryExportException("Could not write dataframe to file %s" % (out_filename))
 
 
 def df_to_csv(df, out_filename):
-    df.to_csv(out_filename)
+    """Writes a Pandas dataframe to a CSV file.
+
+    Args:
+        df (dataframe): dataframe to be written to file
+        filename (str): name of the file
+    Raises:
+        BarryExportException: if file cannot be converted to dataframe
+    """
+
+    try:
+        df.to_csv(out_filename)
+    except Exception as e:
+        raise BarryExportException("Could not write dataframe to file %s" % (out_filename))
